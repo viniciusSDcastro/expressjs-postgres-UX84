a833c9add66c28682c06724db836240dc8095e1c
vinicius <silvadecastrovinicius9@gmail.com>
2022-11-12 19:13:37 -0300

    atualizando

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,9 @@ const pool = new Pool({
 const app = express();
 const port = process.env.PORT || 3333;
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-// app.use(bodyParser.text({ type: "text/html" }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+app.use(bodyParser.text({ type: "text/html" }));
 
 app.use(cors());
 app.use(express.json());
